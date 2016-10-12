0db8c18fee546efef008a8c0ce50d11cfe1162e7
Jose Barrios <cognilab@Joses-MacBook-Pro.local>
2016-10-11 19:30:59 -0700

    Minor patches to MacOS setup file

--- MacOSSetup.sh
+++ MacOSSetup.sh
@@ -4,12 +4,15 @@ cd ~/OSSetup
 #install Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install CMake
+brew install git
 
 #Install solarized color scheme
 git clone https://github.com/altercation/solarized.git
-open solarised/osx-terminal.app-colors-solarized/xterm-256color/Solarized Dark xterm-256color.terminal
+open ~/OSSetup/solarized/osx-terminal.app-colors-solarized/xterm-256color/Solarized\ Dark\ xterm-256color.terminal
+
 
 #Install VIM
-git clone git@github.com:JoseBarrios/vim-dots.git
-./vim-dots/unpack.sh
+git clone https://github.com/JoseBarrios/vim-dots.git
+cd vim-dots
+sh ./unpack.sh
 
