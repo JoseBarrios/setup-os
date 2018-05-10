ed85c79fcc7c09a78a02edd1fad7bf989769d436
Jose Barrios <jose@cognilab.com>
2018-05-09 20:14:25 -0700

    Added Node versioning support

--- MacOSSetup.sh
+++ MacOSSetup.sh
@@ -3,8 +3,14 @@ cd ~/OSSetup
 
 #install Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
 brew install CMake
 brew install git
+brew install node
+# Install n, node version manager
+npm install n -g
+sudo n lts
 
 #Install solarized color scheme
 git clone https://github.com/altercation/solarized.git
@@ -14,7 +20,3 @@ open ~/OSSetup/solarized/osx-terminal.app-colors-solarized/xterm-256color/Solari
 git clone https://github.com/JoseBarrios/vim-dots.git
 cd vim-dots
 sh ./unpack.sh
-
-#Install node
-brew install homebrew/versions/node4-lts
-
