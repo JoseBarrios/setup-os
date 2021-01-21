e11bb3a18338cb247f881379ba81aea6a8886bf9
Jose Barrios <jose@barrios.io>
2021-01-21 14:05:47 -0800

    Updated deployment to include zsh goodies

--- MacOSSetup.sh
+++ MacOSSetup.sh
@@ -9,6 +9,11 @@ brew install CMake
 brew install gpg
 brew install git
 brew install node
+brew upgrade gnupg
+brew install pinentry-mac
+killall gpg-agent
+git config --global gpg.program gpg
+echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
 # Install n, node version manager
 npm install n -g
 sudo n lts
@@ -21,3 +26,13 @@ open ~/OSSetup/solarized/osx-terminal.app-colors-solarized/xterm-256color/Solari
 git clone https://github.com/JoseBarrios/vim-dots.git
 cd vim-dots
 sh ./unpack.sh
+cd ..
+
+# Fonts and icons
+git clone git@github.com:ryanoasis/nerd-fonts.git
+cd nerd-fonts && ./install.sh
+cd ..
+
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
